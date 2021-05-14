3d44cbbd8cddcf6896d2d54260a616d54330bb8c
anisshili <anisshili.as@gmail.com>
2021-05-13 21:50:18 -0400

    Added  actions

---
+++ src/actions/index.js
@@ -0,0 +1,32 @@
+
+export const addAccount = (name, balance, status) => {
+    return{
+        type: 'ADD_ACCOUNT',
+        payload: {
+            name, balance, status
+        }
+    };
+};
+
+export const removeAccount = (accountId) => {
+    return {
+        type: 'REMOVE_ACCOUNT',
+        payload: accountId
+    }
+}
+
+export const switchAccount = (accountId) => {
+    return {
+        type: 'SWITCH_ACCOUNT',
+        payload: accountId
+    }
+}
+
+export const addTransaction = (name, type, amount) => {
+    return {
+        type: 'ADD_TRANSACTION',
+        payload: {
+            name, type, amount
+        }
+    }
+}
